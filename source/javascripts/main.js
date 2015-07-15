efaf40b30c34c539fcfb96da13de39861fb0f3c2
anes901 <anes901@gmail.com>
2015-07-15 05:18:13 +0000

    corrected hero section 'see more' && added fade-in animation

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -5,15 +5,11 @@
 var jump = function(e) {
   e.preventDefault();
 
-  var target = $(this);
-
   $('html, body').animate({
 
-    scrollTop : $(target.parent()).offset().top + 200
+    scrollTop : $("header.hero").height()
 
-  }, 500, function(){
-    location.hash = target;
-  });
+  }, 500);
 }
 
 var jumpUp = function(e) {
@@ -24,9 +20,7 @@ var jumpUp = function(e) {
 
     scrollTop : $(target).parent().prev("section").offset().top - 200
 
-  }, 500, function(){
-    location.hash = target;
-  });
+  }, 500);
 
 }
 
@@ -39,9 +33,7 @@ var jumpDown = function(e) {
 
     scrollTop : $(target).parent().next().offset().top - 200
 
-  }, 500, function(){
-    location.hash = target;
-  });
+  }, 500);
 }
 
 var hero = function(e) {
@@ -51,9 +43,7 @@ var hero = function(e) {
 
   $('html, body').animate({
     scrollTop : $(".hero").offset().top
-  }, 1000, function(){
-    location.hash = target;
-  });
+  }, 1000);
 }
 
 var team = function(e) {
@@ -63,9 +53,7 @@ var team = function(e) {
 
   $('html, body').animate({
     scrollTop : $(".container section#members").offset().top
-  }, 1000, function() {
-    location.hash = target;
-  });
+  }, 1000);
 }
 
 var environment = function(e) {
@@ -75,9 +63,7 @@ var environment = function(e) {
 
   $('html, body').animate({
     scrollTop : $(".container section#environment").offset().top
-  }, 1000, function() {
-    location.hash = target;
-  });
+  }, 1000);
 }
 
 var join = function(e) {
@@ -87,9 +73,7 @@ var join = function(e) {
 
   $('html, body').animate({
     scrollTop : $(".container section#join_us").offset().top
-  }, 1000, function(){
-    location.hash = target;
-  });
+  }, 1000);
 }
 
 $(function(){
@@ -99,12 +83,44 @@ $(function(){
   //src :  http://www.yjn.kr/bbs/board.php?bo_table=prog_html&wr_id=771
 
   var pattern = Trianglify({
-    width : $("canvas").outerWidth(), height : $(document).innerHeight()
+    width : $("canvas").outerWidth(), height : $("body").height()
   });
 
   pattern.canvas(document.getElementById("trianglify"));
 
-  $(".container").css("background-image");
+  //$(".container section").css("opacity", "0");
+
+  $(window).scroll(function(){
+    
+    //$(".container section").animate({
+      //opacity : "1"
+    //}, 20000);
+
+    //src : www.ordinarycoder.com/jquery-fade-content-scroll
+
+    $(".container section").each(function(){
+      if(this.id == "members"){
+        $(".container section#members section").each(function(){
+          $(this).addClass("fadeInBlock");
+        });
+      }
+      else {
+        $(this).addClass("fadeInBlock");
+      }
+    });
+
+    $(".fadeInBlock").each(function(i){
+      var bottom_of_object = $(this).position().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+      //adjust the added number to either have a delay of that the content starts fading a bit before you reach it
+      bottom_of_window = bottom_of_window + 200;
+
+      if(bottom_of_window > bottom_of_object) {
+        $(this).animate({'opacity' : '1'}, 1000);
+      }
+    })
+  });
 
   $(".seeMore").bind("click", jump);
   $(".arrow").bind("click", jumpDown);
@@ -116,7 +132,6 @@ $(function(){
   $("ul.global-nav-list li:nth-child(3)").bind("click", join);
 
   $('#members section').each(function(){
-
     $($(this).find(".profile img")).click(function(){
       $($($(this).closest(".profile")).find("p")).toggle();
       $($($(this).closest(".profile")).find("#example")).toggle();
