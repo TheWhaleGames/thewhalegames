f2882b24b55e9c2c416b0356ed565058ca131d8a
anes901 <anes901@gmail.com>
2015-07-17 04:58:39 +0000

    outline completed

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -46,6 +46,16 @@ var hero = function(e) {
   }, 1000);
 }
 
+var works = function(e) {
+  e.preventDefault();
+
+  var target = $(this);
+
+  $('html, body').animate({
+    scrollTop : $(".container section#service").offset().top
+  }, 1000);
+}
+
 var team = function(e) {
   e.preventDefault();
 
@@ -56,27 +66,17 @@ var team = function(e) {
   }, 1000);
 }
 
-var environment = function(e) {
+var contact = function(e) {
   e.preventDefault();
 
   var target = $(this);
 
   $('html, body').animate({
-    scrollTop : $(".container section#environment").offset().top
+    scrollTop : $(".container section#environment #contact").offset().top
   }, 1000);
 }
 
-var join = function(e) {
-  e.preventDefault();
-
-  var target = $(this);
-
-  $('html, body').animate({
-    scrollTop : $(".container section#join_us").offset().top
-  }, 1000);
-}
-
- //x_colors: ['#ffffff', '#ffffff', '#ffffff', '#f0f0f0', '#e3e3e3', '#d7d7d7', '#cacaca', '#bfbfbf', '#ababab', '#989898', '#8e8e8e', '#848484']
+//x_colors: ['#ffffff', '#ffffff', '#ffffff', '#f0f0f0', '#e3e3e3', '#d7d7d7', '#cacaca', '#bfbfbf', '#ababab', '#989898', '#8e8e8e', '#848484']
 
 $(function(){
 
@@ -90,14 +90,41 @@ $(function(){
   //src :  http://www.yjn.kr/bbs/board.php?bo_table=prog_html&wr_id=771
 
   var pattern = Trianglify({
-    width : $("canvas").width(), height : $("canvas").height(), cell_size: 300, x_colors: ['#bfcccf', '#ced8da', '#ffffff', '#ffffff', '#ffffff', '#ffffff', '#ffffff', '#ffffff', '#ffffff', '#ffffff', '#ffffff'], y_colors: ['#91a7ad','#bfcccf', '#ced8da', '#ffffff', '#ffffff', '#ffffff'], variance: 1
+    width : $("canvas#trianglify").width(), 
+    height : $("canvas#trianglify").height(), 
+    cell_size: 300, 
+    x_colors: ['#bfcccf', '#ced8da', '#ffffff', '#ffffff', '#ffffff', '#ffffff', '#ffffff', '#ffffff', '#ffffff', '#ffffff', '#ffffff'], 
+    y_colors: ['#91a7ad','#bfcccf', '#ced8da', '#ffffff', '#ffffff', '#ffffff'], 
+    variance: 1
   });
 
   var draw_canvas = document.getElementById("trianglify");
 
-
   pattern.canvas(draw_canvas);
 
+  var team_height = $("#members").outerHeight(true);
+  var join_height = $("#join_us").outerHeight(true);
+  var environment_height = $("#environment").outerHeight(true);
+  var total_height = team_height + join_height + environment_height;
+
+  $("#trianglify2").css("height", total_height);
+
+  console.log($("#trianglify2").height());
+
+  var pattern2 = Trianglify({
+    width : $("canvas#trianglify2").width(), 
+    height : $("canvas#trianglify2").height(), 
+    cell_size: 300, 
+    x_colors: ['#989898', '#91a7ad', '#bfcccf', '#ced8da', '#d9e1e2', '#d9e1e2', '#d9e1e2', '#ced8da', '#ced8da', '#bfcccf', '#91a7ad', '#989898'],
+    y_colors: ['#d9e1e2', '#d9e1e2', '#f0f0f0', '#f0f0f0', '#bfcccf', '#91a7ad', '#ffffff']
+  });
+
+  var draw_canvas2 = document.getElementById("trianglify2");
+
+  pattern2.canvas(draw_canvas2);
+
+  var pull_down= $("#environment").css("margin-bottom");
+  $("#trianglify2").css("bottom", -pull_down);
 
   $("img.center_logo").css("margin-left", -$('img.center_logo').width()/2);
   $(".vision").css("margin-left", -$('.vision').width()/2);
@@ -107,62 +134,34 @@ $(function(){
   $(".people_dancing img:odd").addClass("animation_danceReverse").addClass("dance_property");
 
 
-  $("#join_us .group div").each(function(){
+  $("#join_us div").each(function(){
     $(this).mouseenter(function(){
       var target = $(this);
 
       target.css("background-color", "#a32c2c");
-      target.children("h1").css("opacity", "0");
-      target.children("p").css("opacity", "1");
+      target.children("#head").css("opacity", "0");
+      target.children("#content").css("opacity", "1");
     })
     .mouseleave(function(){
       var target = $(this);
 
-      target.css("background-color", "#bbbbbb");
-      target.children("h1").css("opacity", "1");
-      target.children("p").css("opacity", "0");
+      target.css("background-color", "#bbb");
+      target.children("#head").css("opacity", "1");
+      target.children("#content").css("opacity", "0");
     })
   });
 
 
-  $(".responsibility").css("margin-left", -$(".responsibility").width()/2);
   $("#button").css("margin-left", -$("#button").width()/2);
-
-  //$(window).scroll(function(){
-    ////src : www.ordinarycoder.com/jquery-fade-content-scroll
-
-    //$(".container section").each(function(){
-      //if(this.id == "members"){
-        //$(".container section#members section").each(function(){
-          //$(this).addClass("fadeInBlock");
-        //});
-      //}
-      //else {
-        //$(this).addClass("fadeInBlock");
-      //}
-    //});
-
-    //$(".fadeInBlock").each(function(i){
-      //var bottom_of_object = $(this).position().top + $(this).outerHeight();
-      //var bottom_of_window = $(window).scrollTop() + $(window).height();
-
-      ////adjust the added number to either have a delay of that the content starts fading a bit before you reach it
-      //bottom_of_window = bottom_of_window + 200;
-
-      //if(bottom_of_window > bottom_of_object) {
-        //$(this).animate({'opacity' : '1'}, 1000);
-      //}
-    //})
-  //});
 
   $(".scroll").bind("click", jump);
   $(".arrow").bind("click", jumpDown);
   $(".arrowReverse").bind("click", jumpUp);
-  
+
   $(".global-nav-item .logo .banner_logo").bind("click", hero);
-  $("ul.global-nav-list li:first").bind("click", team);
-  $("ul.global-nav-list li:nth-child(2)").bind("click", environment);
-  $("ul.global-nav-list li:nth-child(3)").bind("click", join);
+  $("ul.global-nav-list li:first").bind("click", works);
+  $("ul.global-nav-list li:nth-child(2)").bind("click", team);
+  $("ul.global-nav-list li:nth-child(3)").bind("click", contact);
 
   $('#members section').each(function(){
     $($(this).find(".profile img")).click(function(){
@@ -172,7 +171,34 @@ $(function(){
   });
 
   /*
-  * source : jsfiddle.net/tcloninger/e5qad/
-  */
+   * source : jsfiddle.net/tcloninger/e5qad/
+   */
 
 });
+
+//$(window).scroll(function(){
+////src : www.ordinarycoder.com/jquery-fade-content-scroll
+
+//$(".container section").each(function(){
+//if(this.id == "members"){
+//$(".container section#members section").each(function(){
+//$(this).addClass("fadeInBlock");
+//});
+//}
+//else {
+//$(this).addClass("fadeInBlock");
+//}
+//});
+
+//$(".fadeInBlock").each(function(i){
+//var bottom_of_object = $(this).position().top + $(this).outerHeight();
+//var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+////adjust the added number to either have a delay of that the content starts fading a bit before you reach it
+//bottom_of_window = bottom_of_window + 200;
+
+//if(bottom_of_window > bottom_of_object) {
+//$(this).animate({'opacity' : '1'}, 1000);
+//}
+//})
+//});
