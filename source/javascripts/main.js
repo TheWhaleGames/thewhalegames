03c473a27ee597525a64cfa12ed2da4304499506
anes901 <anes901@gmail.com>
2015-08-03 08:11:32 +0000

    version alert only when IE

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -11,7 +11,7 @@ function msieversion() {
 $(function(){
   var version = msieversion();
 
-  if(version < 10){
+  if($(document).hasClass("ie") && version < 10){
     alert("IE10 이상 또는 다른 브라우저를 이용해주십시오.");
     $("#service").css("opacity", "1");
   }
