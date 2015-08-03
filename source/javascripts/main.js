4f9571c3c3c992c3a6ea377f88bd96bfba6f4690
anes901 <anes901@gmail.com>
2015-08-03 08:04:11 +0000

    handle cases using IE9 and under

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -1,9 +1,27 @@
+function msieversion() {
+  var ua = window.navigator.userAgent;
+  var msie = ua.indexOf("MSIE");
+
+  if(msie > 0)
+    return parseInt(ua.substring(msie+5, ua.indexOf(".", msie)));
+  else
+    return 0;
+}
+
 $(function(){
+  var version = msieversion();
+
+  if(version < 10){
+    alert("IE10 이상 또는 다른 브라우저를 이용해주십시오.");
+    $("#service").css("opacity", "1");
+  }
+
   $(".hero").css("height", $(window).height());
 });
 
 $(document).ready(function(){
   $(window).on('load resize', function(){
+    $(".hero").css("height", $(window).height());
     $(".pattern").css("height", $(".people_dancing img").height()*0.7);
     if($(window).width() >= 1024) {
       activateEffect.memberProfile();
