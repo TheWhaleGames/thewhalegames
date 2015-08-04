2327e1473e69db37743c6132d5fcfe8f2dea488c
anes901 <anes901@gmail.com>
2015-08-04 06:38:29 +0000

    멤버 추가

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -2,9 +2,9 @@ function msieversion() {
   var ua = window.navigator.userAgent;
   var msie = ua.indexOf("MSIE ");
 
-  if (msie > 0)      // If Internet Explorer, return version number
+  if (msie > 0)      
     return parseInt(ua.substring(msie + 5, ua.indexOf(".", msie)));
-  else                 // If another browser, return 0
+  else            
     return 0;
 }
 
