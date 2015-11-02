e06a872dbdcef7e2ef7197efdcaa53ccc8792682
minjoo <mj.kang@thewhalegames.com>
2015-11-02 16:15:08 +0900

    window resize 시 댄서들 마진값 조정 적용

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -11,9 +11,35 @@ $(document).ready(function(){
     $(window).on('load resize', function(){
       $(".hero").css("height", $(window).height());
       $(".pattern").css("height", $(".people_dancing img").height()*0.7);
+
+      member_count = $('.member > section').length;
+
+      if($(window).width() <= 800){
+        $('.people_dancing img').css('display', 'none');
+      }
+      else if($(window).width() <= 900) {
+        $('.people_dancing img').css({
+          'display': 'inline-block',
+          "width": $(window).width()/(member_count*2.4),
+          "height": "auto"
+        });
+      }
+      else
+        {
+          $('.people_dancing img').css({
+          'display': 'inline-block',
+            "width": "auto",
+            "height": "50px"
+          });
+        }
+      space = $(window).width() - member_count*$('.people_dancing img').width();
+      $('.people_dancing img').css('margin', '0 ' + space/(3*member_count) + 'px');
+
       if($(window).width() >= 1024) {
         activateEffect.memberProfile();
       }
+
+      
     });
 
     draw_triangle.drawFirst();
